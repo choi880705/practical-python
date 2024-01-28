0a6664a3f32249e675605aa4c219cb84b187bf3c
LeeKyoungho <goodkorean@gmail.com>
2024-01-29 00:20:46 +0900

    연습문제 1.27 주식매수금액 계산

--- Work/gk61_pcost.py
+++ Work/gk61_pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+"""
+portfolio.csv의 각 컬럼은 보유 종목의 이름, 주식 수, 매수가격에 해당한다. 
+이 파일을 열어 전체 행을 읽은 뒤, 포트폴리오의 전체 주식을 매수하는 데 드는 비용을 계산하는 
+pcost.py 프로그램을 작성한다.
+"""
+
+with open("data/portfolio.csv", "rt") as file:
+    cost = 0
+    headers = next(file)  # 헤더 추출
+    for line in file:
+        # print(line)
+        row = line.split(",")
+        # print(row)
+        cost += int(row[1]) * float(row[2])
+        # print(cost)
+    print("Total cost", cost)

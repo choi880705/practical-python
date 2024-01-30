4dc2ee79630b41d44880229f04d435ee5e1ce625
LeeKyoungho61 <goodkorean@gmail.com>
2024-01-30 15:58:03 +0900

    [연습문제 1.30] 주식매수금액 계산을 함수로 변경

--- Work/gk61_pcost.py
+++ Work/gk61_pcost.py
@@ -1,20 +1,27 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.30
 
 """
-portfolio.csv의 각 컬럼은 보유 종목의 이름, 주식 수, 매수가격에 해당한다. 
-이 파일을 열어 전체 행을 읽은 뒤, 포트폴리오의 전체 주식을 매수하는 데 드는 비용을 계산하는 
-pcost.py 프로그램을 작성한다.
+연습 문제 1.27의 pcost.py 프로그램 코드를 가져다가 portfolio_cost(filename) 함수를 만들어 보라. 
+이 함수는 파일명을 입력으로 받아 포트폴리오 데이터를 읽고, 포트폴리오의 총 비용을 부동소수점으로 반환한다.
 """
 
-with open("data/portfolio.csv", "rt") as file:
-    cost = 0
-    headers = next(file)  # 헤더 추출
-    for line in file:
-        # print(line)
-        row = line.split(",")
-        # print(row)
-        cost += int(row[1]) * float(row[2])
-        # print(cost)
-    print("Total cost", cost)
+
+def portfolio_cost(filename):
+    """매수 파일을 열어서, 매수금액을 계산
+
+    Args:
+        filename (file): 매수 정보가 들어있는 파일
+    """
+    with open(filename, "rt") as f:
+        cost = 0
+        headers = next(f).split(",")  # 헤더 추출, 분리
+        for line in f:  # 모든 라인 처리
+            row = line.split(",")  # 티커, 수량, 가격
+            cost += int(row[1]) * float(row[2])  # 수량 * 가격
+    return cost
+
+
+cost = portfolio_cost("data/portfolio.csv")
+print("Total cost:", cost)
